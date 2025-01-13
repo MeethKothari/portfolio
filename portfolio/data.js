31b6e8470ad68e99753842c092eb921fefff2e51
Meeth <meethukothari@gmail.com>
2025-01-13 11:34:54 +0530

    .

--- portfolio/data.js
+++ portfolio/data.js
@@ -1,37 +1,37 @@
 const data = [
     {
         name: "Qkart",
-        image: "./ProjectImages/qkart.png",
+        image: "./public/qkart.png",
         description: "An Amazon like E-commerce website built on react.js where users can order products online",
         link: `https://meethukothari-me-qkart-frontend-v2.vercel.app/`
     },
     {
         name: "Medify (Appointment Booking)",
-        image: "./ProjectImages/medify.png",
+        image: "./public/medify.png",
         description: "A website where users can book an appointment and book their slots for checkup",
         link: `https://medify-brown.vercel.app/`
     },
     {
         name: "Dessert Booking",
-        image: "./ProjectImages/dessert.png",
+        image: "./public/dessert.png",
         description: "An Zepto like website where users can order desserts online",
         link: `https://product-list-with-cart-main-roan.vercel.app/`
     },
     {
         name: "X-Exit (Resignation)",
-        image: "./ProjectImages/xExit.png",
+        image: "./public/xExit.png",
         description: "An corporate type resignation portal website where employees can resign and HR can approve and get details",
         link: `https://meethukothari-me-mern-xexit.vercel.app/`
     },
     {
         name: "Task-Manager",
-        image: "./ProjectImages/taskManager.png",
+        image: "./public/taskManager.png",
         description: "A Todo application to manage our day to day activity",
         link: `https://task-manager-beta-smoky-31.vercel.app/`
     },
     {
         name: "ChatBot",
-        image: "./ProjectImages/botAi.png",
+        image: "./public/botAi.png",
         description: "A responsive chat Bot application built on react.js where users can chat with...",
         link: `https://botai-orpin.vercel.app/`
     },
